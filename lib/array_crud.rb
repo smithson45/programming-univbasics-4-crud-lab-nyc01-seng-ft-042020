faea757f55827c41be01c2d4d76f64f6260ae1d7
Andre Carter <carstar2486@gmail.com>
2020-04-05 20:02:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,27 +3,32 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  ["Andre","Paul","Craig","Steve"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["Andre","Paul","Craig"]
+  array << "Steve"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = [2,3,4,5]
+  array.unshift(1)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = [5,4,3,2,1]
+  array.pop(1)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = [5,6,7,8,9]
+  array.shift(5)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = [1,2,3,4,5,6,7]
+  element = array[0]
 end
 
 def retrieve_first_element_from_array(array)
