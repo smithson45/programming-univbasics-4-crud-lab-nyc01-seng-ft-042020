121fb79a9c654b39d1095d42149859d94b746f4f
Andre Carter <carstar2486@gmail.com>
2020-04-05 20:14:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,8 +31,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array = [1,2,3,4,5]
-  element = array[4]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
