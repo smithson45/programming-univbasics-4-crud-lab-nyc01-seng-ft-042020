8c9eea83433693f73bc23bae4cee4b247c3f911c
Andre Carter <carstar2486@gmail.com>
2020-04-05 20:06:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Andre","Paul","Craig"]
-  array << "Steve"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
